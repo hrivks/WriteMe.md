f760f4f68a9644a96e3263aca08606c855f7b97e
Hari <hrivks@gmail.com>
2018-10-03 13:32:11 -0400

    Moving from previous repo

---
+++ src/app/components/user/user-list-select/user-list-select.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserListSelectComponent } from './user-list-select.component';
+
+describe('UserListSelectComponent', () => {
+  let component: UserListSelectComponent;
+  let fixture: ComponentFixture<UserListSelectComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserListSelectComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserListSelectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
